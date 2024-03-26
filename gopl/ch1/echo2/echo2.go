b6629c1b230bb8967293bbe32c504eafd7db92c5
lovelydayss <137208403@qq.com>
2024-03-26 19:55:23 +0800

    add gopl-ch1  echo1~3 and dup1~3

---
+++ gopl/ch1/echo2/echo2.go
@@ -0,0 +1,25 @@
+// Echo2 prints its command-line arguments.
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+
+	/*
+
+		s := ""
+		var s string
+		var s = ""
+		var s string = ""
+
+	*/
+}
